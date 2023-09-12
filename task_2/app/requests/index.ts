7951b82d6d886f3fae57a9dd33b2bace0a6ec1b4
Richard Emijere <emijere.richard@gmail.com>
2023-09-12 12:42:20 +0000

    changes

--- task_2/app/requests/index.ts
+++ task_2/app/requests/index.ts
@@ -1,6 +1,6 @@
 // const key = process.env.NEXT_TMDB_APIKEY as string
 
-const key = "f1c2432283129585c2f00f510328087e";
+const key = process.env.NEXT_PUBLIC_TMDB_KEY
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
