2f719e9120fa01e23480cb57160b535dbfe702d5
Richard Emijere <67247138+The-CodeINN@users.noreply.github.com>
2023-09-20 22:53:12 +0000

    Added auth

--- task_3/app/layout.tsx
+++ task_3/app/layout.tsx
@@ -1,15 +1,16 @@
-import Navbar from "@/components/Navbar";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Work_Sans } from "next/font/google";
-import Footer from "@/components/Footer";
+import Navbar from '@/components/Navbar';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Work_Sans } from 'next/font/google';
+import Footer from '@/components/Footer';
+import { ClerkProvider } from '@clerk/nextjs';
 
-const work_sans = Work_Sans({ subsets: ["latin"] });
+const work_sans = Work_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Image Gallery || TheCodeINN",
+  title: 'Image Gallery || TheCodeINN',
   description:
-    "Image gallery with Next.js and Tailwind CSS with search functionality and drag and drop rearranging of images.",
+    'Image gallery with Next.js and Tailwind CSS with search functionality and drag and drop rearranging of images.',
 };
 
 export default function RootLayout({
@@ -18,16 +19,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body
-        className={`${work_sans.className} min-h-screen bg-black-100 text-white`}
-      >
-        <Navbar />
-        <main className='flex-cente paddings mx-auto w-full max-w-screen-2x mt-24'>
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body
+          className={`${work_sans.className} min-h-screen bg-black-100 text-white`}
+        >
+          <Navbar />
+          <main className='flex-cente paddings mx-auto w-full max-w-screen-2x mt-24'>
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- task_3/components/Navbar.tsx
+++ task_3/components/Navbar.tsx
@@ -1,11 +1,15 @@
-"use client"
+'use client';
 
-import Image from "next/image";
-import Link from "next/link";
-import MobileNav from "./MobileNav";
-import SeachInput from "./Seachinput";
+import Image from 'next/image';
+import Link from 'next/link';
+import MobileNav from './MobileNav';
+import SeachInput from './Seachinput';
+
+import { UserButton, useAuth } from '@clerk/nextjs';
 
 const Navbar = () => {
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+
   return (
     <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-black-100'>
       <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16'>
@@ -19,22 +23,19 @@ const Navbar = () => {
           />
         </Link>
 
-        
-          <SeachInput />
-        
+        {/* Show search input only when logged in */}
+        {isLoaded && userId && <SeachInput />}
 
-        <MobileNav />
+        {/* <MobileNav /> */}
 
-        <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
-          <li className='body-text text-gradient_blue-purple !font-bold'>
+        <ul className='flex-center gap-x-3  md:gap-x-10'>
+          <li className='body-text text-gradient_blue-purple !font-bold max-md:hidden'>
             <Link href='/' target='_blank'>
               Home
             </Link>
           </li>
           <li className='body-text text-gradient_blue-purple !font-bold'>
-            <Link href='/' target='_blank'>
-              Login
-            </Link>
+            <UserButton afterSignOutUrl='/' />
           </li>
         </ul>
       </div>
